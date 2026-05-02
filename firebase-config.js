6a47bd436e9db16f0d8dac448e5e354a5b98afed
sehmusaykut1903-creator <sehmusaykut1903@gmail.com>
2026-05-02 08:01:58 +0300

    Update Firebase configuration with new credentials

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,24 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
+
 import {
   getAuth
 } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js";
+
 import {
   getFirestore
 } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-firestore.js";
+
 import {
   getStorage
 } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "BURAYA_FIREBASE_API_KEY",
-  authDomain: "BURAYA_AUTH_DOMAIN",
-  projectId: "BURAYA_PROJECT_ID",
-  storageBucket: "BURAYA_STORAGE_BUCKET",
-  messagingSenderId: "BURAYA_MESSAGING_SENDER_ID",
-  appId: "BURAYA_APP_ID"
+  apiKey: "AIzaSyDjIGcen_K2oqG2sCFF508VzUt1ITvsjyg",
+  authDomain: "arz-ai.firebaseapp.com",
+  projectId: "arz-ai",
+  storageBucket: "arz-ai.firebasestorage.app",
+  messagingSenderId: "464542023476",
+  appId: "1:464542023476:web:2b09014bb99dbba8d66f61"
 };
 
 export const app = initializeApp(firebaseConfig);
